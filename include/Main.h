6d2c974a88bbff212162896913b7c7c391717e5a
Franco Diaz <mi.francko.dr@outlook.com>
2023-09-24 19:33:29 -0500

    Setting up repo

---
+++ include/Main.h
@@ -0,0 +1,15 @@
+﻿// Include header files
+#include <iostream>
+#include <sstream>
+#include <string>
+
+// Include project libraries
+#include <fmt/core.h>
+
+// Declarations
+bool IsNumber(const std::string& string);
+double CalcLogic(char op, double x, double y);
+double add(double x, double y);
+double subtract(double x, double y);
+double multiply(double x, double y);
+double divide(double x, double y);
---
+++ src/Main.cpp
@@ -0,0 +1,93 @@
+﻿#include "Main.h"
+
+int main(int, char**)
+{
+    char op{ ' ' };
+    double result{ 0.0 };
+    fmt::print("**********Calculator**********\n\n");
+    while (true)
+    {
+        std::string input;
+        fmt::print(": ");
+        std::cin >> input;
+
+        if (input == "q" || input == "Q")
+        {
+            fmt::print("\n*******App Terminated*******\n");
+            break;
+        }
+        else if (input == "+" || input == "-" || input == "*" || input == "/")
+        {
+            op = input[0];
+        }
+        else if (IsNumber(input))
+        {
+            double number = std::stod(input);
+            if (op != ' ')
+            {
+                result = CalcLogic(op, result, number);
+                fmt::print("\n= {}\n\n", result);
+                op = ' ';
+            }
+            else
+            {
+                result = number;
+            }
+        }
+        else
+        {
+            fmt::print("Invalid input, try again!\n");
+        }
+    }
+
+    return 0;
+}
+
+bool IsNumber(const std::string& input)
+{
+    std::istringstream iss(input);
+    double number;
+    return (iss >> number) && (iss.peek() == EOF || iss.peek() == '\n');
+    ;
+}
+double CalcLogic(char op, double x, double y)
+{
+    switch (op)
+    {
+    case '+':
+        return add(x, y);
+    case '-':
+        return subtract(x, y);
+    case '*':
+        return multiply(x, y);
+    case '/':
+        if (y != 0)
+        {
+            return divide(x, y);
+        }
+        else
+        {
+            fmt::print("Division by 0 is not allowed.");
+            return 0.0;
+        }
+    default:
+        fmt::print("Invalid operator.");
+        return 0.0;
+    }
+}
+double add(double x, double y)
+{
+    return x + y;
+}
+double subtract(double x, double y)
+{
+    return x - y;
+}
+double multiply(double x, double y)
+{
+    return x * y;
+}
+double divide(double x, double y)
+{
+    return x / y;
+}
